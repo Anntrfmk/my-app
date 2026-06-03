2a0fda0d66d285f6017f88e31ce44c8387044443
Anntrfmk <nntrfmk@gmail.com>
2026-06-03 17:21:35 +0300

    Update vite.config.js

--- .To-do-list/vite.config.js
+++ .To-do-list/vite.config.js
@@ -4,7 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
- base: '/my-app/.To-do-list/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
